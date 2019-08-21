02336ef6cdb24433fa8bd40c5e0b01e0a739acaa
hoangchien97 <hoangducchien97@gmail.com>
2019-08-21 23:46:18 +0700

    DI in NestJS, getAllTasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks = [{name: "Chien"}];
+
+    getAllTasks(){
+        return this.tasks;
+    }
+}
