b9973787c3a428b1fedc03c8f6725801ec787b67
hoangchien97 <hoangducchien97@gmail.com>
2019-08-26 11:03:54 +0700

    validation  CredentialsDto, password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -8,7 +8,7 @@ export class AuthController {
         private readonly authService: AuthService
     ){}
     @Post('/signup')
-    signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
         // console.log(authCredentialsDto); // { username: 'admin', password: '123456' }
         return this.authService.signUp(authCredentialsDto);
     }
