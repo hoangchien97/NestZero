acee3d209eb61c67170ecd7fba90c9f514f3fa59
hoangchien97 <hoangducchien97@gmail.com>
2019-08-26 10:47:35 +0700

    feature s sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService
+    ){}
+    @Post('/signup')
+    signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+        // console.log(authCredentialsDto); // { username: 'admin', password: '123456' }
+        return this.authService.signUp(authCredentialsDto);
+    }
+}
