d3cf7022e12472139f5e8b473ea5e3306f94340b
hoangchien97 <hoangducchien97@gmail.com>
2019-08-28 12:14:42 +0700

    configuring Database on AWS

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
+
+// RDS_... : cấu hình AWS
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '123456',
-    database: 'taskmanagement',
+    type: dbConfig.type,
+    host: process.env.RDS_HOSTNAME || dbConfig.host,
+    port: process.env.PORT || dbConfig.port,
+    username: process.env.RDS_USERNAME || dbConfig.username,
+    password: process.env.RDS_PASSWORD || dbConfig.password,
+    database: process.env.RDS_DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    synchronize: true,
-    logging: true
+    synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+    logging: dbConfig.logging
 }
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
   
-  const serverConfig = config.get('server');
-  console.log(serverConfig);
+  // console.log(serverConfig);
   
 
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
 
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
